4e0648aa2e8c5f2ab38d6b6854680340a8359eab
eduqg <eduardoqgomes@gmail.com>
2020-01-04 20:56:21 -0300

    Changing sort

--- src/components/Pets/index.tsx
+++ src/components/Pets/index.tsx
@@ -21,6 +21,8 @@ export default function Pets({ history, petlist }: Props) {
   const [loading, setLoading] = useState(false);
   const [pets, setPets] = useState(petlist);
 
+  const [sort, setSort] = useState(false);
+
   useEffect(() => {
     console.log('Meus pets');
     console.log(pets);
@@ -63,6 +65,43 @@ export default function Pets({ history, petlist }: Props) {
     setLoading(false);
   }, []); // eslint-disable-line
 
+  useEffect(() => {
+    async function loadInitialPets() {
+      setLoading(true);
+      try {
+        const loggedToken = localStorage.getItem('userTokenAdopets');
+        const sortOption = sort ? 'name' : '-name';
+
+        if (loggedToken) {
+          const response = await api.post('v1/pet/search', {
+            search: {
+              sex_key: 'MALE',
+              size_key: 'S',
+              age_key: 'ADULT',
+            },
+            options: {
+              sort: [sortOption],
+            },
+          }, {
+            headers: {
+              Authorization: `${loggedToken}`,
+            },
+          });
+
+          console.log(response.data.data);
+          setPets(response.data.data.result);
+        } else {
+          console.log('Token not provided');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadInitialPets();
+
+    setLoading(false);
+  }, [sort]); // eslint-disable-line
+
   function verDados() {
     console.log(pets);
   }
@@ -73,10 +112,11 @@ export default function Pets({ history, petlist }: Props) {
       <h1>All pets page</h1>
       {loading && <h4>Loading</h4>}
       <button type="button" onClick={verDados}>Ver Pets</button>
+      <button type="button" onClick={() => setSort(!sort)}>Mudar Ordem</button>
       <ul>
         {pets
           ? (pets.map((pet) => (
-            <li key={pet.id}>pet</li>
+            <li key={pet.id}>{pet.name}</li>
           ))) : (
             <h4>Sem resultados</h4>
           )}
